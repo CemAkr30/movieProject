fdafc8456d1dc0ae2cf6c0f9c4e8d175dd1f080b
CemAkr <107773674+cakar30@users.noreply.github.com>
2022-11-27 18:57:42 +0300

    first commit

---
+++ src/main/java/com/springfilmproject/springfilmproject/SpringFilmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springfilmproject.springfilmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFilmProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springfilmproject/springfilmproject/SpringFilmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfilmproject.springfilmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFilmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
