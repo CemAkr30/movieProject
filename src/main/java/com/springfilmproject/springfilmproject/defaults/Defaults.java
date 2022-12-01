6ae4c38e833ec61d63350986edd4c27741ee714a
CemAkr30 <107773674+cakar30@users.noreply.github.com>
2022-12-01 23:51:30 +0300

    default package düzeltme

--- src/main/java/com/springfilmproject/springfilmproject/defaults/Defaults.java
+++ src/main/java/com/springfilmproject/springfilmproject/defaults/Defaults.java
@@ -1,6 +1,6 @@
 package com.springfilmproject.springfilmproject.defaults;
 
 public class Defaults {
-    public static final String JSON_TRUE="true";
-    public static final String JSON_FALSE="false";
+    public static final String JSON_TRUE="{\"success:true\"}";
+    public static final String JSON_FALSE="{\"success:false\"}";
 }
